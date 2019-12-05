5a81a3269104e72f124597ec35e25ac473873599
Alex <iampopovich@gmail.com>
2019-12-06 01:32:41 +0300

    stage commit
    добавил инсталл для саблайма

--- hello_docker.sh
+++ hello_docker.sh
@@ -11,20 +11,23 @@ sudo apt purge gnome-software* -y
 sudo apt autoremove
 sudo apt autoclean
 
+sudo apt install curl -y
+
 #stage: sublime installation
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
+#stage: nodejs installation
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+
 #stage: repository installation
 sudo apt update 
 sudo apt upgrade -y
-sudo apt install curl -y
 sudo apt install mc -y
 sudo apt install chromium-browser -y
 sudo apt install git -y
 sudo apt install screenfetch -y
-
-#stage: nodejs installation
-curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
+sudo apt install sublime-text -y
+sudo apt install sublime-merge -y
