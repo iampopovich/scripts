7c057e28b6233b4f7e474b9d510033fe79ce3317
Alex <iampopovich@gmail.com>
2019-12-06 01:29:06 +0300

    init commit
    шелл скрипт для конфига сырой установки убунты

---
+++ hello_docker.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+#stage: deinstallation
+sudo apt purge libreoffice-* -y
+sudo apt purge gnome-sudok* -y
+sudo apt purge gnome-mines* -y
+sudo apt purge aisler* -y
+sudo apt purge firefox* -y
+sudo apt purge thunderbird* -y
+sudo apt purge transmission-* -y
+sudo apt purge gnome-software* -y
+sudo apt autoremove
+sudo apt autoclean
+
+#stage: sublime installation
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+#stage: repository installation
+sudo apt update 
+sudo apt upgrade -y
+sudo apt install curl -y
+sudo apt install mc -y
+sudo apt install chromium-browser -y
+sudo apt install git -y
+sudo apt install screenfetch -y
+
+#stage: nodejs installation
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
