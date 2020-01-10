85a73efecfcb59e04da9400064d21c0539ecfbab
Alex <iampopovich@gmail.com>
2020-01-10 12:27:13 +0300

    Update unity.sh

--- unity.sh
+++ unity.sh
@@ -1,3 +1,4 @@
 wget http://beta.unity3d.com/download/292b93d75a2c/UnitySetup-2019.1.0f2
 chmod +x ~/Downloads/UnitySetup-2019.1.0f2
 ~/Downloads/UnitySetup-2019.1.0f2
+sudo apt install libgconf-2-4
