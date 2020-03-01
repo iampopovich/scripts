4d4be0317e418ee5081acb22ce6121b63677d571
Alex <iampopovich@gmail.com>
2020-03-01 23:07:04 +0300

    Create apktool.sh

---
+++ apktool.sh
@@ -0,0 +1,4 @@
+#stage: apktool installation
+sudo curl https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool >> /usr/local/bin/apktool
+sudo wget -O /usr/local/bin/apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar
+sudo chmod +x /usr/local/bin/apktool.jar /usr/local/bin/apktool
