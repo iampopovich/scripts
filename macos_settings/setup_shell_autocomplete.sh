85494538d19befbd58b0410a9b09da3d00ec32a3
Alex <iampopovich@gmail.com>
2020-05-01 00:14:27 +0300

    Create setup_shell_autocomplete.sh

---
+++ macos_settings/setup_shell_autocomplete.sh
@@ -0,0 +1,5 @@
+sudo touch ~/.inputrc
+sudo bash -c 'echo "set completion-ignore-case on" >> .inputrc'
+sudo bash -c 'echo "set show-all-if-ambiguous on">>.inputrc'
+sudo bash -c 'echo "TAB: menu-complete">>.inputrc'
+#also check this : https://www.edwardbeazer.com/turn-auto-complete-on-for-mac-terminal/
