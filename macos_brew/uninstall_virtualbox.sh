4655213864a372fa84fb7bd5cd4fc8cebda697df
Alex <iampopovich@gmail.com>
2020-04-30 23:52:29 +0300

    Create uninstall_virtualbox.sh

---
+++ macos_brew/uninstall_virtualbox.sh
@@ -0,0 +1,13 @@
+brew update
+brew cleanup
+#################################
+brew cask rm virtualbox --force
+brew cask rm vagrant --force
+brew cask rm vagrant-manager --force
+#################################
+rm -rf /Applications/Vagrant* /Applications/VirtualBox
+sudo rm -f /usr/bin/vagrant /usr/local/bin/vagrant
+sudo rm -rf /opt/vagrant /opt/homebrew-cask/Caskroom/va* /opt/homebrew-cask/Caskroom/virtualbox
+rm -rf ~/.vagrant* ~/.docker ~/Virtual*
+rm -rf /usr/local/Caskroom/virtualbox && rm -rf /Applications/VirtualBox.app/
+sudo pkgutil --forget com.vagrant.vagrant
