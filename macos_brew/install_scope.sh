3f2ce826b97b23e154c94e5f78975998f361445b
Alex <iampopovich@gmail.com>
2020-05-01 12:53:46 +0300

    Create install_scope.sh

---
+++ macos_brew/install_scope.sh
@@ -0,0 +1,13 @@
+############DRAFT##############
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+############
+brew cask install sublime
+brew cask install openmw
+brew cask install chrome-browser
+brew cask install appcleaner
+brew cask install dbeaver
+brew cask install postman
+brew cask install android-file-transfer
+brew cask install onyx
+brew cask install openvpn-client #try to configure native vpn connection
+...
