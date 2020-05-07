90b8d61549bcd3f4f790e0c0a361fd4e1fb30af8
Alex <iampopovich@gmail.com>
2020-05-07 22:54:30 +0300

    Update install_scope.sh

--- macos_brew/install_scope.sh
+++ macos_brew/install_scope.sh
@@ -1,13 +1,15 @@
 ############DRAFT##############
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ############
-brew cask install sublime
-brew cask install openmw
+brew cask install sublime-text
+#brew cask install openmw #doesn't work , don't know why
 brew cask install chrome-browser
 brew cask install appcleaner
-brew cask install dbeaver
+brew cask install dbeaver-ce
 brew cask install postman
 brew cask install android-file-transfer
 brew cask install onyx
 brew cask install openvpn-client #try to configure native vpn connection
-...
+brew cask install rdesktop 
+brew install wget
