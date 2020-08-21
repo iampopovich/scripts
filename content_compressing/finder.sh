4698c4ea0638cb3533a03f9d791e033cc77b2ad1
Alex <iampopovich@gmail.com>
2020-08-21 23:02:24 +0300

    search for video content

---
+++ content_compressing/finder.sh
@@ -0,0 +1,5 @@
+#find video section
+find . -type f -name '*.mp4' >> video_mp4.list
+find . -type f -name '*.mov' >> video_mov.list
+find . -type f -name '*.wmv' >> video_wmv.list
+find . -type f -name '*.avi' >> video_avi.list
