eb9feb90e55f4e1874a92ddc3a45788fd8453670
Alex <iampopovich@gmail.com>
2020-10-23 06:40:47 +0300

    Create highscreenPowerIceEvoSetup.sh

---
+++ phoneStationSetups/highscreenPowerIceEvoSetup.sh
@@ -0,0 +1,12 @@
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall --user 0 com.google.android.apps.messaging
+adb shell pm uninstall --user 0 com.google.android.music
+adb shell pm uninstall --user 0 com.google.android.apps.docs
+adb shell pm uninstall --user 0 com.google.android.apps.photos
+adb shell pm uninstall --user 0 com.google.android.apps.maps
+adb shell pm uninstall --user 0 com.google.android.apps.docs.oem
+adb shell pm uninstall --user 0 com.google.android.videos
+adb shell pm uninstall --user 0 com.google.android.feedback
+adb shell pm uninstall --user 0 com.google.android.talk
+# adb shell pm uninstall --user 0
+#installation section
