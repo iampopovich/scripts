60d9fc1068f83c65fc3482b320b0dafe18884d5c
Alex <iampopovich@gmail.com>
2021-01-03 02:46:05 +0300

    comments added with echo

--- shell_tutorial/simple_commands.sh
+++ shell_tutorial/simple_commands.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-pwd #show me a current directory
+echo "Current directory is:"
+pwd
+echo "Result of execution of pwd ; whoami"
 pwd ; whoami
+echo "Result of execution of pwd ; ls ; uptime"
 pwd ; ls ; uptime
 getconf ARG_MAX #shows max numver of arguments gor command
+
+
+
