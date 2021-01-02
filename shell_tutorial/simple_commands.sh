d4bf3d7112df560a87100e5c8fe62e05ce51daeb
Alex <iampopovich@gmail.com>
2021-01-03 02:50:56 +0300

    users variables + system variables displaying done

--- shell_tutorial/simple_commands.sh
+++ shell_tutorial/simple_commands.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "User's home directory is : $HOME"
+echo "Let skip dollar sign \$1 lol kek"
 echo "Current directory is:"
 pwd
 echo "Result of execution of pwd ; whoami"
@@ -6,6 +8,9 @@ pwd ; whoami
 echo "Result of execution of pwd ; ls ; uptime"
 pwd ; ls ; uptime
 getconf ARG_MAX #shows max numver of arguments gor command
-
+echo ""
+grade=10
+person="Pepe"
+echo "$person is a good boy, he is in grade $grade"
 
 
